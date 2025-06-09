59054712d02b3a9dfd5d4457993c7793eef4a405
sanikalavhate <lavhatesanika0@gmail.com>
2025-06-09 10:48:53 +0530

    pyramid

---
+++ numberprint.java
@@ -0,0 +1,10 @@
+class numberprint{
+    public static void main(String[] args) {
+        
+        for(int i=10;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+        
+    }
+}
---
+++ reverse.java
@@ -0,0 +1,14 @@
+ public class reverse{
+    public static void main(string[] args) {
+        for (int i=5;i>=1;i--){
+            for(int j=1;j<=5;j++){
+                if(j<6-i){
+                    System.out.print();
+                }
+                else
+                    System.out.print();
+            }
+            System.out.println();
+        }
+    }
+}
