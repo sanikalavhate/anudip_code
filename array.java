7e3d5bfa167709f95b89b900d0d919198cce0def
sanikalavhate <lavhatesanika0@gmail.com>
2025-06-09 11:56:14 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int a = 0; a < number.length; a++) {
+            System.out.println(number[a]);
+        }
+    }
+}
